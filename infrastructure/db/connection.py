0571bc9f24206d87944a4aaa1241dff5bd81e23a
Глеб <glebporfirev08@gmail.com>
2025-11-08 20:03:45 +0300

    подправил импорты в connection

--- infrastructure/db/connection.py
+++ infrastructure/db/connection.py
@@ -1,6 +1,9 @@
 from persistent.db.base import Base
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from persistent.db.link import Link
+from persistent.db.link_usage import LinkUsage
+
 _url = "/db.sqlite"
 
 
@@ -13,12 +16,5 @@ def sqlite_connection() -> async_sessionmaker[AsyncSession]:
 def create_all_tables() -> None:
     engine = create_engine(f"sqlite://{_url}", connect_args={"check_same_thread": False})
 
-    # Эти импорты важны для регистрации моделей
-    from persistent.db.link import Link
-    from persistent.db.link_usage import LinkUsage
-
-    print(f"🔄 Создаем таблицы в: {_url}")
-    print(f"📊 Зарегистрированные таблицы: {list(Base.metadata.tables.keys())}")
-
     Base.metadata.create_all(engine)
-    print("✅ Таблицы созданы!")
+
